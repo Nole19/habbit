cbe47fcb807af7a7d62f8e4f4ccbd644efbbbddb
Nole19 <81470173+Nole19@users.noreply.github.com>
2025-01-21 20:18:49 +0100

    First upload
    First upload

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    AZURE_DATABASE_CONNECTION = (
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=tcp:tracker666.database.windows.net,1433;"
+        f"DATABASE={os.getenv('DB_NAME')};"
+        f"UID={os.getenv('DB_USER')};"
+        f"PWD={os.getenv('DB_PASSWORD')};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+    SECRET_KEY = "supersecretkey"
